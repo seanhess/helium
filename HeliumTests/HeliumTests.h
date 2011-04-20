f57b32bb185bd2baafdc444754a24eb56e0d64cb
Sean Clark Hess <seanhess@gmail.com>
2011-04-20 09:13:46 -0400

    Project

---
+++ HeliumTests/HeliumTests.h
@@ -0,0 +1,17 @@
+//
+//  HeliumTests.h
+//  HeliumTests
+//
+//  Created by Sean Hess on 4/20/11.
+//  Copyright 2011 I.TV. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface HeliumTests : SenTestCase {
+@private
+    
+}
+
+@end
